57de057ec067d6f5a302db99cdf3194925a5bb3e
arjav0703 <arjavjain0703@gmail.com>
2025-07-07 17:40:40 +0530

    respond to ping

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("error: {e}");
             }
         }
     }
