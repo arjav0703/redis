ab603f015043dbc44daffb913394e96de5026c75
Arjav Jain <arjavjain0703@gmail.com>
2025-11-01 22:18:13 +0530

    store geolocation

--- src/db_handler/geo.rs
+++ src/db_handler/geo.rs
@@ -1,15 +1,15 @@
 use super::*;
 
 pub async fn add(
-    _db: &Arc<tokio::sync::Mutex<HashMap<String, KeyWithExpiry>>>,
+    db: &Arc<tokio::sync::Mutex<HashMap<String, KeyWithExpiry>>>,
     items: &[RespValue],
     handler: &mut RespHandler,
 ) -> Result<()> {
     // > GEOADD places -0.0884948 51.506479 "London"
-    let _key = items[1].as_string().unwrap();
+    let key = items[1].as_string().unwrap();
     let longitude = items[2].as_string().unwrap().parse::<f64>()?;
     let latitude = items[3].as_string().unwrap().parse::<f64>()?;
-    let _member = items[4].as_string().unwrap();
+    let member = items[4].as_string().unwrap();
 
     let (longitude_valid, latitude_valid) =
         (validate_longitude(longitude), validate_latitude(latitude));
@@ -23,7 +23,38 @@ pub async fn add(
         return Ok(());
     }
 
-    handler.write_value(RespValue::Integer(1)).await?;
+    let mut db = db.lock().await;
+
+    if let Some(entry) = db.get_mut(&key) {
+        match &mut entry.value {
+            crate::types::ValueType::SortedSet(vec) => {
+                if let Some(existing) = vec.iter_mut().find(|(m, _)| m == &member) {
+                    existing.1 = 0.0;
+                    handler.write_value(RespValue::Integer(0)).await?;
+                } else {
+                    vec.push((member.clone(), 0.0));
+                    handler.write_value(RespValue::Integer(1)).await?;
+                }
+            }
+            _ => {
+                handler
+                    .write_value(RespValue::SimpleError(
+                        "WRONGTYPE Operation against a key holding the wrong kind of value".into(),
+                    ))
+                    .await?;
+                return Ok(());
+            }
+        }
+    } else {
+        db.insert(
+            key.clone(),
+            KeyWithExpiry {
+                value: crate::types::ValueType::SortedSet(vec![(member.clone(), 0.0)]),
+                expiry: None,
+            },
+        );
+        handler.write_value(RespValue::Integer(1)).await?;
+    }
     Ok(())
 }
 
